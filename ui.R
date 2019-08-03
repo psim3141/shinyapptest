2cc8d04089ebde87b8d8c3467a35be961340f517
Patrick Simon <psimon3141@googlemail.com>
2019-08-03 16:08:17 +0900

    Update ui.R to include documentation

--- ui.R
+++ ui.R
@@ -14,10 +14,35 @@ shinyUI(fluidPage(
                         textOutput("intOut")
                 ),
                 mainPanel(
-                        h1("Select your fitting window"),
-                        plotOutput("plot1", brush = brushOpts(
-                                id = "brush1"
-                        ))
+                        tabsetPanel(type = "tabs", 
+                                    tabPanel("Documentation", br(), 
+                                             h1("Documentation"),
+                                             p("This app lets you play around
+                                                with the Swiss Fertility dataset
+                                                that is included with R. You can
+                                                plot the Fertility rate in various
+                                                states, against one of four possible
+                                                predictors. You can choose whether you
+                                                want to look at all states, or only at
+                                                Catholic/Protestant majority states.
+                                                Then you can make a linear fit to the
+                                                displayed data by selecting a range
+                                                of the points. The output slope and
+                                                intercept will be displayed in the
+                                                side bar. Please use the tabs above
+                                                to switch between this documentation
+                                                and the output window. Enjoy!"),
+                                             br(),p("Written by Patrick Simon, 
+                                                    August 3rd 2019")),
+                                    tabPanel("Output", br(),
+                                             h1("Select your fitting window"),
+                                             plotOutput("plot1", 
+                                                        brush = brushOpts(
+                                                                id = "brush1"
+                                                                )
+                                                        )
+                                    )
+                        )
                 )
         )
 ))
